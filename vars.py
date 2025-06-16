44e3933c7c01cf3456405031cfa886833dc0bf6f
Theboys23456 <krishna93107437@gmail.com>
2025-06-16 15:35:44 +0530

    Create vars.py

---
+++ vars.py
@@ -0,0 +1,25 @@
+#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+
+import os
+
+from os import environ
+
+
+
+API_ID = int(environ.get("API_ID", "26328207"))
+
+API_HASH = environ.get("API_HASH", "d6dcc13df7527b36df3a28e04a72b35b")
+
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+
+OWNER = int(environ.get("OWNER", "6126688051"))
+
+CREDIT = "STRANGER BOY"
+
+LOG_CHANNEL = "-1002335220633" # log group me bot ko admin narur banaye full rights ke sath
+
+#WEBHOOK = True  # Don't change this
+
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
