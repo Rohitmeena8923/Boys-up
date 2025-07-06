2e03f4971eafc0ca5a716e35129884ec8653fa55
Rohitmeena8923 <157653731+Rohitmeena8923@users.noreply.github.com>
2025-07-06 22:00:05 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -8,17 +8,17 @@ from os import environ
 
 
 
-API_ID = int(environ.get("API_ID", "20928172"))
+API_ID = int(environ.get("API_ID", "27775431"))
 
-API_HASH = environ.get("API_HASH", "48ed56c8db54f85d232f576b150360ef")
+API_HASH = environ.get("API_HASH", "b70bb1d45a1d05236671d4cc615e40f9")
 
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "6443740402"))
+OWNER = int(environ.get("OWNER", "6414266397"))
 
 CREDIT = "Shreya"
 
-LOG_CHANNEL = "-1002556958706" # log group me bot ko admin narur banaye full rights ke sath
+LOG_CHANNEL = "-2446676469" # log group me bot ko admin narur banaye full rights ke sath
 
 #WEBHOOK = True  # Don't change this
 
